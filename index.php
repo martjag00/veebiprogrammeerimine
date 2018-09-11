6a3ece499151ce36448c1cc6ab6b6ae8bd8ce49d
Martin Jagodin <martjag00@users.noreply.github.com>
2018-09-11 14:27:34 +0300

    magama

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Igor Smirnov</title>
+  </head>
+  <body>
+    <h1>Martin Jagodin</h1>
+	<p>Selle lehe <a href="http://www.tlu.ee" target="_blank">TLÜ</a> tegin kiiruga tunnis õppimise jaoks ning ei oma mingit väärtusliku sisu.</p>
+	<p>Selle teksti kirjutasin kodus läpakas, sest tahtsin proovida kas saan kõike teha ka kodus.</p>
+	<img src="https://www.pexels.com/photo/clown-fish-swimming-128756/.jpg" alt="">
+	<p>Minu sõber teeb ka <a href="../../~danilat" target="_blank">lingi</a>
+  </body>
+</html>	
