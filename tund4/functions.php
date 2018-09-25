fdd16bcb3c7b36502a3f9f37869336f93015c262
Martin Jagodin <martjag00@users.noreply.github.com>
2018-09-25 15:21:31 +0300

    4. tunni töö, andmebaasi ühendamine leheküljega

---
+++ tund4/functions.php
@@ -0,0 +1,36 @@
+<?php
+ require("../../../config.php");
+ $database="if18_martin_ja_1";
+ //echo $serverHost;
+
+ function saveamsg($msg){
+	$notice="";
+	//loome andmebaasiühenduse
+	$mysqli= new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"],$GLOBALS["serverPassword"],$GLOBALS["database"]);
+	//valmistan ette andmebaasikäsu
+	$stmt=$mysqli->prepare("INSERT INTO vpamsg (message) VALUES(?)");
+	echo $mysqli->error;
+	//asendan ettevalmistatud käsus küsimärgi(d) päris andmetega
+	//esimesena kirja andmetüübid, siis andmed ise
+	//s - string; i- integer; d- decimal
+	$stmt->bind_param("s", $msg);
+	//täidame ettevalmistatud käsu
+	if ($stmt->execute()){
+	$notice= 'Sõnum: "' .$msg . '" on edukalt salvestatud!';
+	} else {
+	$notice="Sõnumi salvestamisel tekkis tõrge: " .$stmt->error;
+	}
+	//sulgeme ettevalmistatud käsu
+	$stmt->close();
+	//sulgeme ühenduse
+	$mysqli->close();
+	return $notice;
+ }	 
+ //teksti sisendi kontrollimine
+ function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+ }
+?>
